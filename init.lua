f3e4304a2e4686ea62aeff902605cd1c4c840a8c
pokeypen7 <bryanbadilla77@gmail.com>
2025-12-13 18:59:16 -0800

    Moved all added plugins to /lua/custom/plugins/init.lua and enabled move window keybinds

--- init.lua
+++ init.lua
@@ -239,10 +239,10 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
 -- NOTE: [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
@@ -320,94 +320,6 @@ require('lazy').setup({
       },
     },
   },
-
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-  },
-
-  {
-    'arnamak/stay-centered.nvim',
-    lazy = false,
-    opts = {
-      skip_filetypes = {},
-      disable_on_mouse = false,
-    },
-  },
-
-  {
-    "smoka7/multicursors.nvim",
-    event = "VeryLazy",
-    dependencies = {
-        'nvimtools/hydra.nvim',
-    },
-    opts = {},
-    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
-    keys = {
-              {
-                mode = { 'v', 'n' },
-                '<Leader>m',
-                '<cmd>MCstart<cr>',
-                desc = '[M]ultiCursor Start',
-            },
-    },
-  },
-
-  {
-    "jiaoshijie/undotree",
-    -- -@module 'undotree.collector'
-    -- -@type UndoTreeCollector.Opts
-    opts = {
-      -- your options
-    },
-    keys = {
-      { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", desc = "[U]ndoTree" }
-    },
-  },
-
-  {
-    "NeogitOrg/neogit",
-    lazy = true,
-    dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      -- "sindrets/diffview.nvim",        -- optional - Diff integration
-
-      -- Only one of these is needed.
-      -- "nvim-telescope/telescope.nvim", -- optional
-      "ibhagwan/fzf-lua",              -- optional
-      -- "nvim-mini/mini.pick",           -- optional
-      -- "folke/snacks.nvim",             -- optional
-    },
-    cmd = "Neogit",
-    keys = {
-      { "<leader>g", "<cmd>Neogit<cr>", desc = "[N]eogit UI" }
-    },
-  },
-
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-      opts = {
-        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-        multiwindow = false, -- Enable multiwindow support.
-        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        line_numbers = true,
-        multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
-        -- Separator between context and content. Should be a single character string, like '-'.
-        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-        separator = nil,
-        zindex = 20, -- The Z-index of the context window
-        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-      },
-  },
-
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
@@ -604,59 +516,59 @@ require('lazy').setup({
     end,
   },
 
-  {
-    "ahmedkhalf/project.nvim",
-    config = function()
-      require("project_nvim").setup {
-
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- Manual mode doesn't automatically change your root directory, so you have
-        -- the option to manually do so using `:ProjectRoot` command.
-        manual_mode = false,
-
-        -- Methods of detecting the root directory. **"lsp"** uses the native neovim
-        -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
-        -- order matters: if one is not detected, the other is used as fallback. You
-        -- can also delete or rearangne the detection methods.
-        detection_methods = { "lsp", "pattern" },
-
-        -- All the patterns used to detect root dir, when **"pattern"** is in
-        -- detection_methods
-        patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
-
-        -- Table of lsp clients to ignore by name
-        -- eg: { "efm", ... }
-        ignore_lsp = {},
-
-        -- Don't calculate root dir on specific directories
-        -- Ex: { "~/.cargo/*", ... }
-        exclude_dirs = {},
-
-        -- Show hidden files in telescope
-        show_hidden = false,
-
-        -- When set to false, you will get a message when project.nvim changes your
-        -- directory.
-        silent_chdir = false,
-
-        -- What scope to change the directory, valid options are
-        -- * global (default)
-        -- * tab
-        -- * win
-        scope_chdir = 'global',
-
-        -- Path where project.nvim will store the project history for use in
-        -- telescope
-        datapath = vim.fn.stdpath("data"), -- refer to the configuration section below
-
-        -- Telescope Integration
-        -- require('telescope').load_extension('projects'),
-        -- require'telescope'.extensions.projects.projects{},
-      }
-    end
-  },
-
+  -- {
+  --   "ahmedkhalf/project.nvim",
+  --   config = function()
+  --     require("project_nvim").setup {
+  --
+  --       -- your configuration comes here
+  --       -- or leave it empty to use the default settings
+  --       -- Manual mode doesn't automatically change your root directory, so you have
+  --       -- the option to manually do so using `:ProjectRoot` command.
+  --       manual_mode = false,
+  --
+  --       -- Methods of detecting the root directory. **"lsp"** uses the native neovim
+  --       -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
+  --       -- order matters: if one is not detected, the other is used as fallback. You
+  --       -- can also delete or rearangne the detection methods.
+  --       detection_methods = { "lsp", "pattern" },
+  --
+  --       -- All the patterns used to detect root dir, when **"pattern"** is in
+  --       -- detection_methods
+  --       patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+  --
+  --       -- Table of lsp clients to ignore by name
+  --       -- eg: { "efm", ... }
+  --       ignore_lsp = {},
+  --
+  --       -- Don't calculate root dir on specific directories
+  --       -- Ex: { "~/.cargo/*", ... }
+  --       exclude_dirs = {},
+  --
+  --       -- Show hidden files in telescope
+  --       show_hidden = false,
+  --
+  --       -- When set to false, you will get a message when project.nvim changes your
+  --       -- directory.
+  --       silent_chdir = false,
+  --
+  --       -- What scope to change the directory, valid options are
+  --       -- * global (default)
+  --       -- * tab
+  --       -- * win
+  --       scope_chdir = 'global',
+  --
+  --       -- Path where project.nvim will store the project history for use in
+  --       -- telescope
+  --       datapath = vim.fn.stdpath("data"), -- refer to the configuration section below
+  --
+  --       -- Telescope Integration
+  --       -- require('telescope').load_extension('projects'),
+  --       -- require'telescope'.extensions.projects.projects{},
+  --     }
+  --   end
+  -- },
+  --
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
